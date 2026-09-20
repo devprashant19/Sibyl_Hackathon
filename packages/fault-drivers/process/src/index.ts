2c10ce007cfb67d776d92e91cb320fd17133add5
Dev Prashant <dev.prashant1901@gmail.com>
2026-09-20 14:17:18 +0530

    chore(fault-drivers): refactor package dependencies
    Additional updates:
    - feat(fault-drivers): refactor process driver implementation (index)

--- packages/fault-drivers/process/src/index.ts
+++ packages/fault-drivers/process/src/index.ts
@@ -1,4 +1,4 @@
-import type { FaultDriver, DriverContext } from '@sibyl-core';
+import type { FaultDriver, DriverContext } from '@sibyl/core';
 import { wrapChildProcess } from './process-wrapper';
 
 export class ProcessFaultDriver implements FaultDriver {
